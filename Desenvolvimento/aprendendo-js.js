05883438f8a0f7ab008b3434aa3795371262d0ec
Henrique Machado Martins <machadomartinshenrique@gmail.com>
2023-06-01 18:05:44 -0300

    hoje foi dia de aprender mais coisa sobre o JavaScript

---
+++ Desenvolvimento/aprendendo-js.js
@@ -0,0 +1,99 @@
+                      /// Exercicíos6///
+//const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let result = 0;
+//for (let index = 0; index < numbers.length; index += 1) {
+  //if (numbers[index] % 2 !== 0) {
+    //result += 1;
+  //}
+//};
+//if (result === 0) {
+  //console.log('Nenhum valor ímpar encontrado');
+//} else {
+  //console.log(result);
+//};
+
+                      /// Exercicíos5///
+//const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// Inicie a variável com o primeiro valor do array. Assim, é possível pular a posição 0 e iniciar o for com index = 1.
+//let higherNumber = numbers[0];
+//for (let index = 1; index < numbers.length; index += 1) {
+  //if (numbers[index] > higherNumber) {
+    //higherNumber = numbers[index];
+  //}
+//};
+
+//console.log(higherNumber);
+
+                      /// Exercicíos4///
+//const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let result = 0;
+//for (let index = 0; index < numbers.length; index += 1) {
+  //result += numbers[index];
+//};
+//result = result / numbers.length;
+//if (result > 20) {
+  //console.log('O valor da média aritmética é maior que 20');
+//} else {
+  //console.log('O valor da média aritmética é menor ou igual a 20');
+//};
+
+                      /// Execicío 3 ///
+//const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let sum = 0;
+//for (let index = 0; index < numbers.length; index += 1) {
+  //sum += numbers[index];
+//};
+//let average = sum / numbers.length;
+//console.log(average);
+
+                      /// Execicío2 ///
+
+//const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let result = 0;
+//for (let index = 0; index < numbers.length; index += 1) {
+  //result += numbers[index];
+//};
+//console.log(result);
+
+
+                      /// Execicío 1 ///
+
+//const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//for (let index = 0; index < numbers.length; index += 1) {
+  //console.log(numbers[index]);
+//};
+
+                      ///Final do exercicíos///
+
+            //const shopping = ['Sabão em pó', 'Macarrão', 'Leite condesado', 'Refrigerante', 'Maça', 'Alface', 'Nescau']
+            //for ( let index = 0;  index < shopping.length; index += 1){
+            //console.log(index);
+            //console.log(shopping[index]);
+            //console.log(`${index} - ${shopping[index]}`);
+            //};
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////                            
+            //const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+            //for (let index = 0; index < groceryList.length; index += 1) {
+            //const currentItem = groceryList[index];
+            //console.log(currentItem);
+            //};
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //const menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+            //const menuServices = '';
+            //const menuServices = menu[1];
+            //menu.push('Contato');
+            //console.log(menu);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //console.log(shopping);
+            //console.log(shopping[0]);
+            //console.log(shopping[shopping.length - 1]);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //shopping.push('Estilete'); "ele é acrecentado no ultimo número"
+            //console.log(shopping);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //shopping.unshift('Milho'); "ele é acrecentado no primeiro número"
+            //console.log(shopping);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //shopping.pop(); "ele retira o ultimo número"
+            //shopping.shift(); "ele retira o primeiro número"
+            //console.log(shopping);
